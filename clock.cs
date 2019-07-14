9f5818b08dc0cd2392c109da5da76bd0f15847aa
ronazuelos <52877731+ronazuelos@users.noreply.github.com>
2019-07-14 11:52:13 +0300

    Add files via upload

---
+++ clock.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClock
+{
+    class clock
+    {
+        private static clock INSTANCE;
+        private static object key = new object();
+
+        private clock()
+        { }
+
+        public static clock getInstance()
+        {
+            if (INSTANCE != null)
+            {
+               return INSTANCE;
+            }
+            lock (key)
+            {
+                if (INSTANCE == null)
+                {
+                    INSTANCE = new clock();
+                }
+
+                return INSTANCE;
+            }
+        }
+
+        public void printTime()
+        {
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
+
