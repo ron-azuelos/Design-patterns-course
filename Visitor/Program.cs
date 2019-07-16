fefd50185a9ee6feee4e17987a999f998015ddbb
ronazuelos <52877731+ronazuelos@users.noreply.github.com>
2019-07-16 10:21:20 +0300

    Add files via upload

---
+++ Visitor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Liquer
+
+            Liquor vodka = new Liquor(95);
+
+            Console.WriteLine(vodka);
+
+            IVisitor endOfYearVisitor = new EndOfYearTax();
+
+            Console.WriteLine($"Price after tax = {vodka.Accept(endOfYearVisitor)}");
+
+            // Perfume
+
+            Perfume per = new Perfume(500);
+
+            Console.WriteLine(per);
+
+            IVisitor OneIlsVisitor = new OneILSSale();
+
+            Console.WriteLine($"Price after tax = {per.Accept(OneIlsVisitor)}");
+
+
+            // ShopRent
+
+            ShopRent shop = new ShopRent(5000);
+
+            Console.WriteLine(shop);
+
+            Console.WriteLine($"Price after tax = {shop.Accept(endOfYearVisitor)}");
+
+        }
+    }
+}
---
+++ Visitor/ShopRent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor
+{
+    public class ShopRent : IVisitable
+    {
+        public double price;
+        
+        public ShopRent(double nPrice) 
+        {
+            price = nPrice;
+        }
+
+         public double Accept(IVisitor visitor)
+         {
+             return visitor.Visit(this);
+         }
+        public override string ToString()
+        {
+            return $"Shop Rent price : {price}";
+        }
+    }
+}
