d8d85e491265585344ab765ccd0ed16e9796ee04
ronazuelos <52877731+ronazuelos@users.noreply.github.com>
2019-07-16 15:44:54 +0300

    Add files via upload

---
+++ Template/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new OracleSelectAllQuery().Run();
+
+            Console.WriteLine();
+
+            new OracleUpdateQuery().Run();
+
+            Console.WriteLine();
+
+            new MongoGetQuery().Run();
+
+            Console.WriteLine();
+
+            new MongoDeleteQuery().Run();
+
+            Console.WriteLine();
+
+            new MongoInsertQuery().Run();
+        }
+    }
+}
