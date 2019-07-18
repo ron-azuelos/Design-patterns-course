445314f1bbab9e0e84528bbc6d2b783ee46aa53c
ronazuelos <52877731+ronazuelos@users.noreply.github.com>
2019-07-18 14:18:10 +0300

    Add files via upload

---
+++ Memento/Context.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Memento
+{
+    public class Context
+    {
+
+        TextCareTaker careTake = new TextCareTaker();
+
+        public void Save(RichTextBox richTex)
+        {
+            careTake.AddMemento(new TextMemento(richTex.Text));
+        }
+
+        public void Load(RichTextBox richText)
+        {
+            //if (careTake.GetLast() != null && careTake.GetLast().savedText != null)
+                richText.Text = careTake.GetLast().savedText;
+        }
+
+        public void Redo(RichTextBox richText)
+        {
+            //if (careTake.GetNext() != null && careTake.GetNext().savedText != null)
+                richText.Text = careTake.GetNext().savedText;
+        }
+    }
+}
---
+++ Memento/TextCareTaker.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    class TextCareTaker
+    {
+        List<TextMemento> lMemento = new List<TextMemento>();
+        int index = -1;
+
+        public void AddMemento(TextMemento memento)
+        {
+            if ((lMemento.Count > 1) & (index < lMemento.Count - 1))
+            {
+                lMemento.RemoveRange(index + 1, lMemento.Count - index - 1);
+            }
+            
+            this.lMemento.Add(memento);
+            index++;
+        }
+
+        public TextMemento GetLast()
+        {
+
+            if ((index > -1) & (lMemento.Count > 0))
+            {
+                index--;
+                return this.lMemento[index + 1];
+            }
+
+            return new TextMemento("Error you can't do undo !");
+            
+            
+        }
+
+        public TextMemento GetNext()
+        {
+
+            if((index < lMemento.Count - 1 ) & ((lMemento.Count > 0)))
+            {
+                index++;
+                return this.lMemento[index];
+            }
+
+            return new TextMemento("Error you can't do redo !");
+            
+        }
+    }
+}
