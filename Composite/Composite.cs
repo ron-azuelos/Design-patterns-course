e68bdd874c5d86a41cef8154817b58be00a81a01
ronazuelos <52877731+ronazuelos@users.noreply.github.com>
2019-07-18 14:13:23 +0300

    Add files via upload
    Including 2 exercises: Composite & CompositBtree

---
+++ Composite/Composite.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    public class Composite : Component
+    {
+        IList<Component> childs = new List<Component>();
+
+        public Composite(string name) : base(name)
+        {
+        }
+
+        public override void AddChild(Component c)
+        {
+            childs.Add(c);
+        }
+
+        public override IList<Component> GetChilds()
+        {
+            return childs;
+        }
+
+        public override void RemoveChild(Component c)
+        {
+            childs.Remove(c);
+        }
+
+        public override void Draw(string space)
+        {
+            Console.WriteLine(space + this.name);
+            foreach (Component c in childs)
+            {
+                c.Draw(space + "    ");
+            }
+        }
+
+        public override int CountLeaf()
+        {
+            int count = 0;
+            foreach (Component child in childs)
+            {
+                count += child.CountLeaf();
+            }
+            return count;
+        }
+
+        public override bool IsBinary()
+        {
+            if ((childs.Count == 2) || (childs.Count == 0))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Composite/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Component root = new Composite("Canvas root");
+            Component circle1 = new Leaf("Circle 1");
+            Component rectangle = new Leaf("Rectangle x");
+            root.AddChild(circle1);
+            root.AddChild(rectangle);
+
+            Component container1 = new Composite("Container 1");
+            Component circle2 = new Leaf("Circle 2");
+            Component circle3 = new Leaf("Circle 3");
+            container1.AddChild(circle2);
+            container1.AddChild(circle3);
+
+            root.AddChild(container1);
+
+            Component container2 = new Composite("Container of triangles");
+            Component t1 = new Leaf("Triangle 1");
+            Component t2 = new Leaf("Triangle 2");
+            container2.AddChild(t1);
+            container2.AddChild(t2);
+
+            root.AddChild(container2);
+
+            root.Draw("");
+
+            Console.WriteLine($"Leaf number of root :  { root.CountLeaf() } \n");
+            Console.WriteLine($"Leaf number of container 1 : { container1.CountLeaf() } \n");
+            Console.WriteLine($"Leaf number of container 2 : { container2.CountLeaf() } \n");
+            Console.WriteLine($"Leaf number of Triangle 1 :  { t1.CountLeaf() } \n");
+
+            Console.WriteLine("Is binary root : " + IsBinary(root));
+            Console.WriteLine("Is binary container 1: " + IsBinary(container1));
+
+        }
+
+        static bool IsBinary(Component c)
+        {
+            if (!c.IsBinary())
+            {
+                return false;
+            }
+
+            if (c.GetChilds() == null || c.GetChilds().Count == 0)
+            {
+                return true;
+            }
+
+            return IsBinary(c.GetChilds()[0]) && IsBinary(c.GetChilds()[1]);
+        }
+    }
+}
